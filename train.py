08e3b43dc8864f3186795f7387b2444c750a9d91
EC2 Default User <ec2-user@ip-10-0-28-237.ec2.internal>
2022-10-21 01:40:46 +0000

    update train.py

--- train.py
+++ train.py
@@ -8,10 +8,10 @@ import mlflow
 import mlflow.keras
 
 # Add mlflow logging
+mlflow.autolog()
 
-
-input_dir =
-target_dir =
+input_dir = os.path.join("data","images")
+target_dir = os.path.join("data","annotations","trimaps")
 img_size = (160, 160)
 num_classes = 3
 batch_size = 32
@@ -25,7 +25,7 @@ target_img_paths = sorted([
 ])
 
 # Free up RAM in case the model definition cells were run multiple times
-
+keras.backend.clear_session()
 
 # Build model
 model = get_model(img_size, num_classes)
@@ -46,10 +46,19 @@ val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_target_img_p
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
 
 # Set the keras callback using ModelCheckpoint to save the best results of the model
-callbacks =
+callbacks = keras.callbacks.ModelCheckpoint(
+    filepath="checkpoints/",
+    save_weights_only=True,
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True)
 
 # Train the model, doing validation at the end of each epoch.
 # Make sure to include the calllback for mlflow
 epochs = 15
-model.fit(train_gen, epochs=epochs, validation_data=val_gen)
+with mlflow.start_run() as run:
+    model.fit(train_gen, epochs=epochs, validation_data=val_gen)
 model.save('./segmentation/')
+
+print("run_id: {}".format(r.info.run_id))
+print("metrics: {}".format(r.data.metrics))
